6fd8aa83bfd42cb9e8b7d03701420d4524fbe4c5
yttiiz <dominique.talis@gmail.com>
2022-07-19 02:43:14 +0200

    burger & menu animation almost done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 //==========================| Elements |==========================//
-const burger = document.getElementById('burger')
 
 //==========================| Events |==========================//
 burger.addEventListener('click', moveMenu)
 
 //==========================| Functions |==========================//
 function moveMenu() {
-    const menu = document.querySelector('.nav-container nav')
-    menu.classList.toggle('move')
+    const burger = document.querySelector('#burger button'),
+    container = document.querySelector('.nav-container'),
+    navigation = container.querySelector('nav'),
+    spans = burger.querySelectorAll('span')
+
+    container.classList.toggle('dark')
+    navigation.classList.toggle('move')
+    
+    spans.forEach((span, index) => {
+        switch(index) {
+            case 0:
+                span.classList.toggle('line1')
+                break
+
+            case 1:
+                span.classList.toggle('line2')
+                break
+
+            case 2:
+                span.classList.toggle('line3')
+                break
+        }
+    })
 }
