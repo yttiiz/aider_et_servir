48a2677078fd411ec05f37dc364ed45603f8464a
yttiiz <dominique.talis@gmail.com>
2022-08-01 01:14:18 +0200

    challenge done - :-)

--- js/form-validation.js
+++ js/form-validation.js
@@ -6,12 +6,12 @@ form.addEventListener('submit', fakeSubmitData)
 
 //==========================| Function |==========================//
 function fakeSubmitData(e) {
-    const divInputsContainers = [].slice.call(e.target.children).filter(item => item.nodeName === 'DIV')
+    const divInputsList = [].slice.call(e.target.children).filter(item => item.nodeName === 'DIV')
     let checkPoint = 0
 
     e.preventDefault()
     
-    divInputsContainers.forEach(div => {
+    divInputsList.forEach(div => {
         const input = div.children[1]
 
         if (input.value) {
@@ -22,8 +22,8 @@ function fakeSubmitData(e) {
         }
     })
 
-    if (checkPoint === divInputsContainers.length) {
-        divInputsContainers.forEach(div => div.children[1].value = '')
+    if (checkPoint === divInputsList.length) {
+        divInputsList.forEach(div => div.children[1].value = '')
         alert('Merci pour votre message ! \nNous vous contactons très rapidement.')
 
         return console.log('Données envoyées au serveur !')
